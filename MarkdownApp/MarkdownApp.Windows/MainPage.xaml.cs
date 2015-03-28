9efbeee16ed456174760c2397f1f1a4cdd4802c5
Tobias Schulz <tobias.schulz186@gmail.com>
2015-03-28 22:21:48 +0100

    rtf

--- MarkdownApp/MarkdownApp.Windows/MainPage.xaml.cs
+++ MarkdownApp/MarkdownApp.Windows/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.Storage.Provider;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -47,6 +48,16 @@ namespace MarkdownApp
                 // Load the file into the Document property of the RichEditBox.
                 editor.Document.LoadFromStream(Windows.UI.Text.TextSetOptions.FormatRtf, randAccStream);
             }
+        }
+
+        private async void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                    new CoreDispatcherPriority(), () =>
+                    {
+
+                        RtfConversion.bla(editor.Document);
+                    });
         }
 
         private async void SaveButton_Click(object sender, RoutedEventArgs e)
