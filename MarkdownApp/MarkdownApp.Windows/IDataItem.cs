a0d645d10d4c89064a1ae051505dee9fcd2c3eb3
Tobias Schulz <tobias.schulz186@gmail.com>
2015-05-01 01:08:28 +0200

    dataitem

---
+++ MarkdownApp/MarkdownApp.Windows/IDataItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownApp
+{
+    interface IDataItem
+    {
+        string UniqueId { get; }
+        string Title { get; }
+        string Subtitle { get; }
+        string Description { get; }
+        string ImagePath { get; }
+        string Content { get; }
+    }
+}
